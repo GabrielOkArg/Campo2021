3e844b2610aecf942a761a9a2e41cf48b72cdf4d
Gabriel Silva <silvagabriel21@yahoo.com.ar>
2021-09-05 11:22:17 -0300

    se sube solucion con acquitectura armada

---
+++ CTH.Interfaces/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTH.Interfaces
+{
+    public class Class1
+    {
+    }
+}
